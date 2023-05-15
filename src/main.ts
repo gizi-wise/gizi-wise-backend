16410458ce6d1dcdb95ab703eabcb866acd19687
Imam Fahrur Rofi <11655799-masfahru@users.noreply.gitlab.com>
2023-05-15 09:02:21 +0700

    feat: use hyper-express as platform

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HyperExpressAdapter } from './hyper-express-adapter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new HyperExpressAdapter());
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   await app.listen(port, '0.0.0.0');
