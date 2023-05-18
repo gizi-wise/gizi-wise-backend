14a5a8524383dd0e61b02566262b5e9840a3273a
Imam Fahrur Rofi <imamfahrurrofi@gmail.com>
2023-05-18 16:21:29 +0700

    test: a simple test if gcloud replace instances is worked

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Gizi Wise API is online';
+    return 'Gizi Wise API is online!';
   }
 }
