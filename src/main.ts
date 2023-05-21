b914758de8144e83484701601c1d6eb0ab293de2
Imam Fahrur Rofi <imamfahrurrofi@gmail.com>
2023-05-21 07:40:48 +0700

    feat: add cors and helmet to reduce xss attack probabilities

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HyperExpressAdapter } from './hyper-express-adapter';
+import * as cors from 'cors';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new HyperExpressAdapter());
@@ -12,6 +14,18 @@ async function bootstrap() {
     defaultVersion: [VERSION_NEUTRAL, '1'],
   });
   const configService = app.get(ConfigService);
+  const nodeEnv = configService.get('NODE_ENV');
+  const corsOptionsDelegate = (req, res) => {
+    if (nodeEnv === 'production') {
+      res(null, { origin: /giziwise\.my\.id$/ });
+    } else {
+      res(null, { origin: '*' });
+    }
+  };
+  app.use(cors(corsOptionsDelegate));
+  if (nodeEnv === 'production') {
+    app.use(helmet());
+  }
   const port = configService.get('PORT');
   await app.listen(port, '0.0.0.0');
   Logger.log(`Server running on port ${port}`, 'Bootstrap');
