1e95cbde517448c6aaeef8b4ebd8bd1f0c765ad6
Imam Fahrur Rofi <imamfahrurrofi@gmail.com>
2023-05-27 17:15:32 +0700

    feat: create common function for dto validation

---
+++ src/common/functions/validateAndTransformData.ts
@@ -0,0 +1,17 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import { validateSync } from 'class-validator';
+import { Model } from 'sequelize-typescript';
+
+export function validateAndTransformData(data: any, omit: string[] = []) {
+  Object.assign(this, data instanceof Model ? data.toJSON() : data);
+  const errors = validateSync(this, {
+    validationError: { target: true },
+    whitelist: true,
+  });
+  if (errors.length > 0) {
+    throw new InternalServerErrorException(errors);
+  }
+  if (omit.length) {
+    omit.forEach((key) => delete this[key]);
+  }
+}
